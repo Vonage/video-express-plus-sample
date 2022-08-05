69575db3c181f5f7b09d48e7d02b79298adcec46
Jeff Swartz <jeff.swartz@vonage.com>
2022-08-05 12:58:28 -0700

    fix linting errors

--- Vanilla/js/call.js
+++ Vanilla/js/call.js
@@ -1,3 +1,3 @@
-import "vve-breakout-ot/vve-breakout";
-import "vve-text-chat-ot/vve-text-chat";
-import "vve-waiting/vve-waiting";
+import 'vve-breakout-ot/vve-breakout';
+import 'vve-text-chat-ot/vve-text-chat';
+import 'vve-waiting/vve-waiting';
